fce23db71bcb3f4de15d2b660e563562466a1549
daisuke0530 <daisuke0530@gmail.com>
2020-11-03 16:02:52 +0900

    init registry

--- initialize.py
+++ initialize.py
@@ -11,27 +11,27 @@ CALLBACKS = Registry('callbacks')
 METRICS = Registry('metrics')
 TRANSFORMS = Registry('transforms')
 
-# losses
-for k, v in torch.nn.__dict__.items():
-    if 'Loss' in k:
-        if callable(v) and isinstance(v, type):
-            LOSSES.register(v)
-
-# optimizers
-for k, v in torch.optim.__dict__.items():
-    if callable(v):
-        OPTIMIZERS.register(v)
-
-# schedulers
-for k, v in torch.optim.lr_scheduler.__dict__.items():
-    if callable(v):
-        SCHEDULERS.register(v)
-
-# transforms
-for k,v in albumentations.__dict__.items():
-    if callable(v) and isinstance(v, type):
-        TRANSFORMS.register(v)
-
 def initialize():
     from models import san
     from schedulers import HalfCosineAnnealingLR
+
+    # losses
+    for k, v in torch.nn.__dict__.items():
+        if 'Loss' in k:
+            if callable(v) and isinstance(v, type):
+                LOSSES.register(v)
+
+    # optimizers
+    for k, v in torch.optim.__dict__.items():
+        if callable(v):
+            OPTIMIZERS.register(v)
+
+    # schedulers
+    for k, v in torch.optim.lr_scheduler.__dict__.items():
+        if callable(v):
+            SCHEDULERS.register(v)
+
+    # transforms
+    for k,v in albumentations.__dict__.items():
+        if callable(v) and isinstance(v, type):
+            TRANSFORMS.register(v)
